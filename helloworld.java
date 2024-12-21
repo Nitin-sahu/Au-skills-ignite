d62865f2fe544ee6d563a5243de3aac62a57fb00
Nitin kumar Sahu <65076355+Nitin-sahu@users.noreply.github.com>
2024-12-21 18:32:03 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,24 @@
+public class helloworld
+{
+    public static void  main(String[] args)
+    {
+        System.out.println("hello world");
+            if(int=0 || int >a.length())
+            {
+                system.out.println("index is not valid");
+
+            }
+            int [] c= new int[a.length-1];
+            for(int i=0; i<a.length;i++)
+            {
+                if(i<in)
+                {
+                    c[i]=a[i];
+                }
+
+                else
+                c[i]=a[i++];
+            }
+    }
+    
+}
